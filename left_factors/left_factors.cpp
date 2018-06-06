1c53f59240a59468b27d7a026de3cb730af92d68
NeDimAS <39697940+NeDimAS@users.noreply.github.com>
2018-06-06 22:40:01 +0300

    Update left_factors.cpp

--- left_factors/left_factors.cpp
+++ left_factors/left_factors.cpp
@@ -52,7 +52,7 @@ bool LeftFactors::next (vector <int> & v)
         return true;
     }
     for(int i = n - 2; i > 0; i--){
-        if(v[i - 1] - 1 = v[i] && v[i + 1] - 1 = v[i]){
+        if(v[i - 1] - 1 == v[i] && v[i + 1] - 1 == v[i]){
             v[i] += 2;
             for(int j = i + 1; j < n; j++){
                 if(v[j - 1] == 0){
@@ -62,7 +62,7 @@ bool LeftFactors::next (vector <int> & v)
                     v[j] = v[j - 1] - 1;
                 }
             }
-            return true;
+			return true;
         }
     }
 }
@@ -70,9 +70,9 @@ bool LeftFactors::next (vector <int> & v)
 bool LeftFactors::prev (vector <int> & v)
 {
 	auto n = (int) (v.size ());
-	vector w(n);
+	vector <int> w(n);
 	w[0] = 0;
-	for(int i=1;i<n:i++){
+	for (int i = 1; i < n; i++) {
 	    w[i] = 1 - w[i - 1];
 	}
     if(w == v){
@@ -101,7 +101,7 @@ bool LeftFactors::prev (vector <int> & v)
 vector <vector <int> > LeftFactors::generate_all (int n)
 {
 	vector <vector <int>> ans;
-	vector v(n);
+	vector <int> v(n);
 	v[0] = 0;
 	for(int i = 1; i < n; i++){
 	    v[i] = 1 - v[i - 1];
@@ -115,7 +115,7 @@ vector <vector <int> > LeftFactors::generate_all (int n)
 
 vector <int> LeftFactors::object_by_number (int n, int64_t k)
 {
-	vector v(n);
+	vector <int> v(n);
 	v[0] = 0;
 	for(int i = 1; i < n; i++){
 	    v[i] = 1 - v[i - 1];
@@ -130,9 +130,16 @@ vector <int> LeftFactors::object_by_number (int n, int64_t k)
 int64_t LeftFactors::number_by_object (vector <int> const & v)
 {
 	auto n = (int) (v.size ());
+	vector <int> w(n);
+	w[0] = 0;
+	for (int i = 1; i < n; i++) {
+		w[i] = 1 - w[i - 1];
+	}
+	bool l;
 	int ans = 0;
-	while(LeftFactors::prev(v)){
-        ans++;
+	while (v != w) {
+		l = LeftFactors::next(w);
+		ans++;
 	}
 	return ans;
 }
@@ -140,4 +147,3 @@ int64_t LeftFactors::number_by_object (vector <int> const & v)
 LeftFactors::~LeftFactors ()
 {
 }
-
